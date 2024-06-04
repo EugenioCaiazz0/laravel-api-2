2effccdded312c3aaec7c9a8060f8fc51a61acb5
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-06-04 22:31:42 +0200

    Api/PageController + chiamata json Projects

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class PageController extends Controller
-{
+{//*il compact è inutile perchè aggiunge un livello superfluo (se abbiamo una sola chiave)
+ //*invece in caso di più chiavi è necessario -> return response()->json($key1,$key2)
     public function index() {
-        return response()->json([ //*il compact è inutile perchè aggiunge un livello superfluo (se abbiamo una sola chiave)
-            //*invece in caso di più chiavi è necessario -> return response()->json($key1,$key2)
-            'name' => 'Er Pupone'
-        ]);
+       $projects = Project::all();
+
+       return response()->json($projects);
     }
 }
